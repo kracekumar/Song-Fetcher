e63da9141a0126daf3f81af6ca06c5f5752fc489
kracekumar <kracethekingmaker@gmail.com>
2011-03-16 20:07:35 +0530

    Now the script will parse uri and get the album name and create a directory if it doesn't exist and then scan the directory and start downloading the files and print the time taken to read and write the file to disk

--- yuvan_songs.py
+++ yuvan_songs.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 import urllib2
 import os
-import urllib
+import sys
 from sgmllib import SGMLParser
+import time
 link=raw_input("Enter the URI:")
 content=urllib2.urlopen(link)
 source=content.read()
@@ -19,18 +20,23 @@ url=URLLister()
 url.feed(source)
 content.close()
 url.close()
-dir_content=[]
-dir_content=os.listdir("songs")
-os.chdir("songs")
+dir_name=link.split('/')[-1].split('.')[0]
+dir_content=list(os.listdir(os.getcwd()))
+if not dir_name in dir_content:
+	ret_code=os.mkdir(dir_name)
+os.chdir(dir_name)
+dir_content=list(os.listdir(os.getcwd()))
 y=[x for x in url.urls if x.endswith(".mp3")]
 for all in y:
 	name=all.rsplit('/')[-1]
 	if not  dir_content.__contains__(name):
-		print "-"*50+"\n"+all+"is downloading"
+		print "-"*50+"\n"+name+" is downloading in "+dir_name
+		read_start_time=time.time()
 		file=urllib2.urlopen(all).readlines()
+		read_end_time=time.time()
 		writing=open(name,"w")
 		writing.writelines(file)
+		close_time=time.time()
 		writing.close()
 		dir_content.append(all)
-		print "\n "+all+"Downloaded"
-#os.system("wget '%s'"%(y[0]))
+		print "\n "+name+" took "+str((read_end_time -read_start_time)/60.0)+" minutes to read "+str((close_time - read_end_time)/60.0) +" minutes to write to directory => "+dir_name
